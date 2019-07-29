717db02b78b308eddcc46810f56f1ab132f43df3
James Santucci <james.santucci@gmail.com>
2019-07-29 16:30:44 -0400

    Add cli basics (#4)

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,36 @@
 module Main where
 
-import           Lib
+import qualified Data.ByteString     as BS
+import           Data.Semigroup      ((<>))
+import           Options.Applicative
+
+data Options = Options { inLanguage  :: String
+                       , outLanguage :: String
+                       , inFile      :: String
+                       , outFile     :: String } deriving Show
+
+options :: Parser Options
+options = Options
+  <$> strOption ( long "in-language"
+              <> short 'i'
+              <> metavar "INLANGUAGE"
+              <> help "Language of the input file" )
+  <*> strOption ( long "out-language"
+                  <> short 'o'
+                  <> metavar "OUTLANGUAGE"
+                  <> help "Language of the output file ")
+  <*> argument str ( metavar "INPUTFILE" )
+  <*> argument str ( metavar "OUTPUTFILE" )
+
+run :: Options -> IO ()
+run (Options _ _ inf outf ) = do
+  inText <- BS.readFile inf
+  BS.writeFile outf inText
 
 main :: IO ()
-main = someFunc
+main = run =<< execParser opts
+  where
+    opts = info (options <**> helper)
+      ( fullDesc
+        <> progDesc "Convert from one language's record types to another's"
+        <> header "panrec - like pandoc, but for record types" )
